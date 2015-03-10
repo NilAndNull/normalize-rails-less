d255aa7c67a5cb31141bdaf07bd9c3d8f0a7d044
NilAndNull <NilAndNull@users.noreply.github.com>
2015-03-10 11:30:36 +0000

    Update and rename normalize-rails.rb to normalize-rails-less.rb

---
+++ lib/normalize-rails-less.rb
@@ -0,0 +1,10 @@
+require "normalize-rails-less/version"
+require "normalize-rails-less/engine"
+
+module Normalize
+  module Rails
+    module Less
+    # Your code goes here...
+    end
+  end
+end
